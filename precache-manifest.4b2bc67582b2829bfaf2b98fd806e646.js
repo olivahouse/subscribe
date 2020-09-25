d0a9255a2ddfa744a853c528232834962fd80b34
Marc <mwgdavies@gmail.com>
2020-09-25 16:45:19 +0200

    Updates

---
+++ precache-manifest.4b2bc67582b2829bfaf2b98fd806e646.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08654953de03f03ba229a47c756d080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f489a613fbfd3377c58c",
+    "url": "/static/css/main.db7c8dc9.chunk.css"
+  },
+  {
+    "revision": "b3deac5a68f63a871658",
+    "url": "/static/js/2.43a04c73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43a04c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f489a613fbfd3377c58c",
+    "url": "/static/js/main.df42f503.chunk.js"
+  },
+  {
+    "revision": "0cf5b4e5129282245695",
+    "url": "/static/js/runtime-main.0a0ddeb2.js"
+  }
+]);
